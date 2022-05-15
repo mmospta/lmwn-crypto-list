24da294f09601b05ee7e1de973f59f5161f8c506
Phonthep Aungkanukulwit <phonthep.aungkanukulwit@scbtechx.io>
2022-05-15 14:45:42 +0700

    cryptp-list: add paging api

--- Crypto-List/Crypto-List/Utillities/NetworkManager.swift
+++ Crypto-List/Crypto-List/Utillities/NetworkManager.swift
@@ -12,8 +12,9 @@ class NetworkManager {
     static let shared = NetworkManager()
     
     private let baseURL = "https://api.coinranking.com"
-//limit: Int = 10, 
+    
     func getCoin(completion: @escaping (Result<CoinResponse, AFError>) -> Void) {
+        URLCache.shared = URLCache(memoryCapacity: 0, diskCapacity: 0, diskPath: nil)
         let headers: HTTPHeaders = [
             "x-access-token": "coinranking05d52d3adfd70374eb462a8b63050f3a3651c9886bcf28d3"
         ]
@@ -21,5 +22,20 @@ class NetworkManager {
         AF.request(baseURL + "/v2/coins", method: .get, parameters: parameters, encoder: .urlEncodedForm, headers: headers).responseDecodable(of: CoinResponse.self) { response in
             completion(response.result)
         }
+    }
+    
+    func fetchCoin(limit: Int = 10, offset: Int, completion: @escaping (Result<CoinResponse, AFError>) -> Void) {
+        URLCache.shared = URLCache(memoryCapacity: 0, diskCapacity: 0, diskPath: nil)
+        let headers: HTTPHeaders = [
+            "x-access-token": "coinranking05d52d3adfd70374eb462a8b63050f3a3651c9886bcf28d3"
+        ]
+        let parameters = [
+            "limit": "\(limit)",
+            "offset": "\(offset)"
+        ]
+        AF.request(baseURL + "/v2/coins", method: .get, parameters: parameters, encoder: .urlEncodedForm, headers: headers).responseDecodable(of: CoinResponse.self) { response in
+            completion(response.result)
+        }
+        
     }
 }
