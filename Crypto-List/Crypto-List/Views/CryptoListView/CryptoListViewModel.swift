c73eb7f8548e34e3845a07627c5daa8ea430331b
Phonthep Aungkanukulwit <phonthep.aungkanukulwit@scbtechx.io>
2022-05-15 14:45:59 +0700

    cryptp-list: add paging api

--- Crypto-List/Crypto-List/Views/CryptoListView/CryptoListViewModel.swift
+++ Crypto-List/Crypto-List/Views/CryptoListView/CryptoListViewModel.swift
@@ -10,17 +10,40 @@ import Foundation
 class CryptoListViewModel: ObservableObject {
     @Published var coins: [Coin] = []
     @Published var isLoading = false
+    @Published var isShowingSheet = false
+    @Published var coinDetail: Coin?
     
     func getCrytoCoin() {
         isLoading = true
         NetworkManager.shared.getCoin { [weak self] result in
-            switch result {
-            case .success(let response):
-                self?.coins = response.data.coins
-                self?.isLoading = false
-            case .failure(let error):
-                print(error)
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let response):
+                    self?.coins = response.data.coins
+                    self?.isLoading = false
+                case .failure(let error):
+                    print(error)
+                }
             }
         }
+    }
+    
+    func fetchCoin(offset: Int) {
+        isLoading = true
+        NetworkManager.shared.fetchCoin(offset: offset) { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let response):
+                    self?.coins.append(contentsOf: response.data.coins)
+                    self?.isLoading = false
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        }
+    }
+    
+    func clearCoins() {
+        coins = []
     }
 }
